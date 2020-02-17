a0a7a3199797d0fbe351f7b74af3b98d10cc581e
ggl924620591 <924620591@qq.com>
2020-02-17 08:41:23 +0800

    公用上传图片方法。

---
+++ src/main/java/com/xnx3/wangmarket/shop_storeadmin/controller/CommonController.java
@@ -0,0 +1,48 @@
+package com.xnx3.wangmarket.shop_storeadmin.controller;
+
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xnx3.j2ee.pluginManage.controller.BasePluginController;
+import com.xnx3.j2ee.service.SqlService;
+import com.xnx3.j2ee.util.ActionLogUtil;
+import com.xnx3.j2ee.util.AttachmentUtil;
+import com.xnx3.j2ee.util.Page;
+import com.xnx3.j2ee.util.Sql;
+import com.xnx3.j2ee.vo.UploadFileVO;
+
+
+/**
+ * 公共的一些方法的控制器
+ * @author 关光礼
+ */
+@Controller(value="ShopStoreAdminCommonController")
+@RequestMapping("/shop/storeadmin/common")
+public class CommonController extends BasePluginController {
+	@Resource
+	private SqlService sqlService;
+	
+	/**
+	 * 上传图片接口
+	 */
+	@RequestMapping(value="uploadImage${url.suffix}", method = RequestMethod.POST)
+	@ResponseBody
+	public UploadFileVO uploadImage(Model model,HttpServletRequest request){
+		UploadFileVO uploadFileVO = AttachmentUtil.uploadImage("goodsList/", request, "image", 0);
+		
+		if(uploadFileVO.getResult() == UploadFileVO.SUCCESS){
+			//上传成功，写日志
+			ActionLogUtil.insert(request, "商品详情里图片上传", uploadFileVO.getPath());
+		}
+		
+		return uploadFileVO;
+	}
+}
