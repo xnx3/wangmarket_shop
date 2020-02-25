6a681b3e91fa01c307e10422bf899698632fd0b6
xnx3 <mail@xnx3.com>
2020-02-25 11:46:32 +0800

    1. 优化 CarouselImage 的 type 注释 2. CarouselImageVO 返回值去除没用的字段

--- src/main/java/com/xnx3/wangmarket/shop/vo/CarouselImageVO.java
+++ src/main/java/com/xnx3/wangmarket/shop/vo/CarouselImageVO.java
@@ -1,8 +1,10 @@
 package com.xnx3.wangmarket.shop.vo;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.xnx3.j2ee.vo.BaseVO;
 import com.xnx3.wangmarket.shop.entity.CarouselImage;
+import com.xnx3.wangmarket.shop.vo.bean.CarouselImageBean;
 
 /**
  * 首页显示的轮播图列表
@@ -10,14 +12,26 @@ import com.xnx3.wangmarket.shop.entity.CarouselImage;
  *
  */
 public class CarouselImageVO extends BaseVO{
-	private List<CarouselImage> list;	//轮播图列表，已经根据rank排序好的
+	private List<CarouselImageBean> list;	//轮播图列表，已经根据rank排序好的
 
-	public List<CarouselImage> getList() {
+	public List<CarouselImageBean> getList() {
 		return list;
 	}
 
 	public void setList(List<CarouselImage> list) {
-		this.list = list;
+		List<CarouselImageBean> beanList = new ArrayList<CarouselImageBean>();
+		if(list != null){
+			for (int i = 0; i < list.size(); i++) {
+				CarouselImage carouselImage = list.get(i);
+				CarouselImageBean carouselImageBean = new CarouselImageBean();
+				carouselImageBean.setImageUrl(carouselImage.getImageUrl());
+				carouselImageBean.setImgValue(carouselImage.getImgValue());
+				carouselImageBean.setName(carouselImage.getName());
+				carouselImageBean.setType(carouselImage.getType());
+				beanList.add(carouselImageBean);
+			}
+		}
+		this.list = beanList;
 	}
 
 	@Override
