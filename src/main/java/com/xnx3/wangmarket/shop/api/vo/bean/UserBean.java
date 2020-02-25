b12070548564801b72f062e2234b4c24c6ff987d
xnx3 <mail@xnx3.com>
2020-02-25 20:01:03 +0800

    UserBean 编辑

--- src/main/java/com/xnx3/wangmarket/shop/api/vo/bean/UserBean.java
+++ src/main/java/com/xnx3/wangmarket/shop/api/vo/bean/UserBean.java
@@ -11,10 +11,6 @@ public class UserBean {
 	private String username;	//用户名，限制40字符
 	private String head;		//头像,图片文件名，如 29.jpg
 	private String nickname;	//昵称
-	private Integer regtime;	//注册时间,时间戳
-	private Integer lasttime;	//最后登录时间,时间戳
-	private String regip;		//注册ip
-	private String lastip;		//最后一次登陆的ip
 	private String phone;		//手机号
 	
 	public Integer getId() {
@@ -40,30 +36,6 @@ public class UserBean {
 	}
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
-	}
-	public Integer getRegtime() {
-		return regtime;
-	}
-	public void setRegtime(Integer regtime) {
-		this.regtime = regtime;
-	}
-	public Integer getLasttime() {
-		return lasttime;
-	}
-	public void setLasttime(Integer lasttime) {
-		this.lasttime = lasttime;
-	}
-	public String getRegip() {
-		return regip;
-	}
-	public void setRegip(String regip) {
-		this.regip = regip;
-	}
-	public String getLastip() {
-		return lastip;
-	}
-	public void setLastip(String lastip) {
-		this.lastip = lastip;
 	}
 	public String getPhone() {
 		return phone;
