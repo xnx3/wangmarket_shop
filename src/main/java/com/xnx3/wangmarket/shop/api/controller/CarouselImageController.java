fac12a5ccf7e519e75b6615c34703dc2e3088592
xnx3 <mail@xnx3.com>
2020-02-29 09:49:18 +0800

    修复首页轮播图storeid参数

--- src/main/java/com/xnx3/wangmarket/shop/api/controller/CarouselImageController.java
+++ src/main/java/com/xnx3/wangmarket/shop/api/controller/CarouselImageController.java
@@ -5,7 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.xnx3.BaseVO;
 import com.xnx3.j2ee.pluginManage.controller.BasePluginController;
 import com.xnx3.j2ee.service.SqlService;
 import com.xnx3.j2ee.util.ActionLogUtil;
@@ -23,22 +26,28 @@ public class CarouselImageController extends BasePluginController {
 	
 	/**
 	 * 查询轮播图列表
+	 * @param storeid 要查看的轮播图是那个商铺的
 	 * @author 关光礼
 	 * @return 当 result:1 成功时，data 返回值：list：轮播图列表
 	 */
 	@ResponseBody
-	@RequestMapping(value="/list${api.suffix}",method= {RequestMethod.POST,RequestMethod.GET})
-	public CarouselImageVO findCarouselImageList(HttpServletRequest request) {
+	@RequestMapping(value="/list${api.suffix}",method= {RequestMethod.POST})
+	public CarouselImageVO findCarouselImageList(HttpServletRequest request,
+			@RequestParam(value = "storeid", required = false, defaultValue = "0") int storeid) {
+		CarouselImageVO vo = new CarouselImageVO();
+		if(storeid < 1){
+			vo.setBaseVO(BaseVO.FAILURE, "请传入storeid参数");
+			return vo;
+		}
 		
 		//查轮播图，按顺序字段查找
-		String sql = "SELECT * FROM shop_carousel_image ORDER BY rank ASC";
+		String sql = "SELECT * FROM shop_carousel_image WHERE storeid = "+storeid+" ORDER BY rank ASC";
 		List<CarouselImage> list = sqlService.findBySqlQuery(sql, CarouselImage.class);
 		
 		//返回实体vo
-		CarouselImageVO vo = new CarouselImageVO();
 		vo.setList(list);
 		//日志记录
-		ActionLogUtil.insert(request, getUserId(), "查看轮播图列表");
+		ActionLogUtil.insert(request, storeid, "查看轮播图列表");
 		return vo;
 	}
 
