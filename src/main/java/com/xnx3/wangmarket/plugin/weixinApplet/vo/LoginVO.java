16bf76348caf2ab81fb28b40e787516b02cded5d
xnx3 <mail@xnx3.com>
2020-06-05 12:19:28 +0800

    微信小程序登录接口优化，优化返回的user信息

--- src/main/java/com/xnx3/wangmarket/plugin/weixinApplet/vo/LoginVO.java
+++ src/main/java/com/xnx3/wangmarket/plugin/weixinApplet/vo/LoginVO.java
@@ -1,15 +1,17 @@
 package com.xnx3.wangmarket.plugin.weixinApplet.vo;
 
 import com.xnx3.wangmarket.shop.core.entity.UserWeiXin;
+import com.xnx3.wangmarket.shop.core.vo.UserVO;
 
 /**
  * 微信小程序登录，返回的响应
  * @author 管雷鸣
  *
  */
-public class LoginVO extends com.xnx3.j2ee.vo.LoginVO{
+public class LoginVO extends UserVO{
 	private UserWeiXin userWeiXin;	//对应 user 的weixin信息
 	private String sessionKey;		//小程序使用code登录之后，会返回一个 sessionKey
+	private String token;			//云商城用户登录的sessionid
 
 	public UserWeiXin getUserWeiXin() {
 		return userWeiXin;
@@ -27,10 +29,18 @@ public class LoginVO extends com.xnx3.j2ee.vo.LoginVO{
 		this.sessionKey = sessionKey;
 	}
 
-	@Override
-	public String toString() {
-		return "LoginVO [userWeiXin=" + userWeiXin + ", sessionKey=" + sessionKey + "]";
+	public String getToken() {
+		return token;
 	}
 
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginVO [userWeiXin=" + userWeiXin + ", sessionKey=" + sessionKey + ", token=" + token + ", getUser()="
+				+ getUser() + ", getResult()=" + getResult() + ", getInfo()=" + getInfo() + "]";
+	}
 	
 }
