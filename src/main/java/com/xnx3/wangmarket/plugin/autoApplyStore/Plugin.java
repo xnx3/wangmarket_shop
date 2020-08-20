f7744e5dd33ba84e036be120419e70da4d4944ed
liupeng <liupeng@x.com>
2020-08-20 18:26:27 +0800

    自助开店
    自助开店

--- src/main/java/com/xnx3/wangmarket/plugin/autoApplyStore/Plugin.java
+++ src/main/java/com/xnx3/wangmarket/plugin/autoApplyStore/Plugin.java
@@ -6,7 +6,7 @@ import com.xnx3.j2ee.pluginManage.PluginRegister;
  * 自助开店
  * @author 管雷鸣
  */
-@PluginRegister(menuTitle = "自助开店",menuHref="/plugin/autoApplyStore/store/list.do", applyToCMS=true, intro="用户可以自助填写信息开通商铺。从哪个店铺过来的用户开店，会自动关联上", version="1.0", versionMin="1.0")
+@PluginRegister(menuTitle = "自助开店",menuHref="/plugin/autoApplyStore//list.jsp", applyToCMS=true, intro="用户可以自助填写信息开通商铺。从哪个店铺过来的用户开店，会自动关联上", version="1.0", versionMin="1.0")
 public class Plugin{
 
 }
---
+++ src/main/java/com/xnx3/wangmarket/plugin/vo/StoreListVO.java
@@ -0,0 +1,40 @@
+package com.xnx3.wangmarket.plugin.vo;
+
+import com.xnx3.j2ee.util.Page;
+import com.xnx3.j2ee.vo.BaseVO;
+import com.xnx3.wangmarket.shop.core.entity.Store;
+
+import java.util.List;
+/**
+ * 开店列表
+ * @author 刘鹏
+ */
+public class StoreListVO extends BaseVO {
+
+    private List<Store> storelist;
+    private Page page;
+
+    public List<Store> getStorelist() {
+        return storelist;
+    }
+
+    public void setStorelist(List<Store> storelist) {
+        this.storelist = storelist;
+    }
+
+    public Page getPage() {
+        return page;
+    }
+
+    public void setPage(Page page) {
+        this.page = page;
+    }
+
+    @Override
+    public String toString() {
+        return "StoreListVO{" +
+                "storelist=" + storelist +
+                ", page=" + page +
+                '}';
+    }
+}
