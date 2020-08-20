a992d3581322a8d9d0f74824c5f1f31acfb6f728
liupeng <liupeng@x.com>
2020-08-20 18:38:33 +0800

    自助开店
    自助开店

--- src/main/java/com/xnx3/wangmarket/plugin/autoApplyStore/controller/store/IndexController.java
+++ src/main/java/com/xnx3/wangmarket/plugin/autoApplyStore/controller/store/IndexController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import com.xnx3.j2ee.util.ActionLogUtil;
 import com.xnx3.j2ee.util.Page;
 import com.xnx3.j2ee.util.Sql;
-import com.xnx3.wangmarket.plugin.vo.StoreListVO;
 import com.xnx3.wangmarket.shop.core.entity.Store;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,17 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.xnx3.j2ee.service.SqlCacheService;
 import com.xnx3.j2ee.service.SqlService;
 import com.xnx3.wangmarket.shop.core.pluginManage.controller.BasePluginController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 /**
  * 自助开店，商家后台
  */
-@Controller(value="AutoApplyStoreStoreIndexPluginApiController")
-@RequestMapping("/plugin/api/autoApplyStore/store/")
+@Controller(value="AutoApplyStoreStoreIndexPluginController")
+@RequestMapping("/plugin/autoApplyStore/store/")
 public class IndexController extends BasePluginController {
 	@Resource
 	private SqlService sqlService;
@@ -34,12 +30,8 @@ public class IndexController extends BasePluginController {
 	 * 查看申请开店列表
 	 * @author 刘鹏
 	 */
-	@ResponseBody
-	@RequestMapping(value = "/list${api.suffix}",method = {RequestMethod.POST})
-	public StoreListVO list(HttpServletRequest request,
-							@RequestParam(value = "everyNumber", required = false, defaultValue = "15") int everyNumber) {
-
-		StoreListVO vo = new StoreListVO();
+	@RequestMapping("/list${url.suffix}")
+	public String list(HttpServletRequest request, Model model) {
 		//创建Sql
 		Sql sql = new Sql(request);
 		//配置查询那个表
@@ -48,7 +40,7 @@ public class IndexController extends BasePluginController {
 		// 查询数据表的记录总条数
 		int count = sqlService.count("shop_store", sql.getWhere());
 		// 配置每页显示15条
-		Page page = new Page(count, everyNumber, request);
+		Page page = new Page(count, 15, request);
 		// 查询出总页数
 		sql.setSelectFromAndPage("SELECT * FROM shop_store ", page);
 		//选择排序方式 当用户没有选择排序方式时，系统默认降序排序
@@ -56,11 +48,11 @@ public class IndexController extends BasePluginController {
 		//按照上方条件查询出该实体总数 用集合来装
 		List<Store> list = sqlService.findBySql(sql, Store.class);
 		// 将信息保存到model中
-		vo.setStorelist(list);
-		vo.setPage(page);
+		model.addAttribute("list", list);
+		model.addAttribute("page", page);
 		//日志记录
 		ActionLogUtil.insert(request, getUserId(), "查看开店申请");
-		return vo;
+		return "plugin/autoApplyStore/list";
 
 	}
 }
