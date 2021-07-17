3146f4ffe341d6fa45298189d048ab6d8de0864e
xnx3 <mail@xnx3.com>
2021-07-17 22:46:00 +0800

    版本改为1.4

--- src/main/java/com/xnx3/wangmarket/shop/core/Global.java
+++ src/main/java/com/xnx3/wangmarket/shop/core/Global.java
@@ -12,7 +12,7 @@ public class Global {
 	/**
 	 * 当前的版本号。
 	 */
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.4";
 
 	/**
 	 * 商家的自定义订单规则的持久化缓存Key {storeid} 便是替换为 store.id
