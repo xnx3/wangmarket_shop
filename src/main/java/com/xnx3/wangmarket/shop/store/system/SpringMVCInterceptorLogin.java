440ce14e48914936c1ebe97c3fb5c9cb82b688fe
xnx3 <mail@xnx3.com>
2021-08-21 15:29:47 +0800

    修复商家后台拦截器，无法让 shop.jar 应用于其他项目中，用于商家登录报错的问题

--- src/main/java/com/xnx3/wangmarket/shop/store/system/SpringMVCInterceptorLogin.java
+++ src/main/java/com/xnx3/wangmarket/shop/store/system/SpringMVCInterceptorLogin.java
@@ -5,9 +5,10 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
+import com.xnx3.j2ee.entity.User;
 import com.xnx3.j2ee.pluginManage.interfaces.SpringMVCInterceptorInterface;
 import com.xnx3.j2ee.vo.BaseVO;
-import com.xnx3.wangmarket.shop.core.entity.Store;
+import com.xnx3.wangmarket.shop.core.Global;
 import com.xnx3.wangmarket.shop.store.util.SessionUtil;
 
 /**
@@ -50,8 +51,9 @@ public class SpringMVCInterceptorLogin implements SpringMVCInterceptorInterface{
 			return false;
 		}
 		
-		Store store = SessionUtil.getStore();
-		if(store == null || store.getId() == null || store.getId() - 0 == 0){
+		
+		User user = SessionUtil.getUser();
+		if(!user.getAuthority().equals(""+Global.STORE_ROLE_ID)){
 			//无权使用，自己没有所管理的店铺
 			response.setHeader("Access-Control-Allow-Origin", "*");
 			response.getWriter().write("{"
