07ecf7378f436fcdbdb577caef53b079308650c6
xnx3 <mail@xnx3.com>
2021-12-08 17:35:47 +0800

    update orderNotice实体类

--- src/main/java/com/xnx3/wangmarket/plugin/orderNotice/entity/OrderNotice.java
+++ src/main/java/com/xnx3/wangmarket/plugin/orderNotice/entity/OrderNotice.java
@@ -23,8 +23,14 @@ public class OrderNotice extends BaseEntity implements java.io.Serializable{
 	public final static Short IS_USE_NO = 0;
 	
 	private Integer id;			//对应 store.id， 是哪个商家的规则
-	private Short isUse;		//是否是在使用， 1使用，0不使用
+	private Short payNotice;	//是否使用下单成功的通知，这里起名虽然是支付，意义是只要成功下单的就会提醒，比如在线支付、线下支付等，都会包含在内。 1使用，0不使用
+	private Short refundNotice;	//是否启用申请退单通知.当用户发起申请退单时，系统会自动给商家的手机发送一条短信，通知商家有顾客退单了，请及时处理。1使用，0不使用
 	private String phone;		//发送短信通知，发送到哪个手机号
+	
+	public OrderNotice() {
+		this.payNotice = IS_USE_NO;
+		this.refundNotice = IS_USE_NO;
+	}
 	
 	@Id
 	@Column(name = "id", unique = true, nullable = false, columnDefinition="int(11) comment '对应 store.id， 是哪个商家的规则'")
@@ -36,15 +42,25 @@ public class OrderNotice extends BaseEntity implements java.io.Serializable{
 		this.id = id;
 	}
 	
-	@Column(name = "is_use", columnDefinition="int(11) comment '是否是在使用， 1使用，0不使用' default '0' ")
-	public Short getIsUse() {
-		return isUse;
+	
+	@Column(name = "pay_notice", columnDefinition="tinyint(2) comment '是否使用下单成功的通知，这里起名虽然是支付，意义是只要成功下单的就会提醒，比如在线支付、线下支付等，都会包含在内。 1使用，0不使用' default '0' ")
+	public Short getPayNotice() {
+		return payNotice;
 	}
 
-	public void setIsUse(Short isUse) {
-		this.isUse = isUse;
+	public void setPayNotice(Short payNotice) {
+		this.payNotice = payNotice;
 	}
 	
+	@Column(name = "refund_notice", columnDefinition="tinyint(2) comment '是否启用申请退单通知.当用户发起申请退单时，系统会自动给商家的手机发送一条短信，通知商家有顾客退单了，请及时处理。1使用，0不使用' default '0' ")
+	public Short getRefundNotice() {
+		return refundNotice;
+	}
+
+	public void setRefundNotice(Short refundNotice) {
+		this.refundNotice = refundNotice;
+	}
+
 	@Column(name = "phone", columnDefinition="char(15) comment '发送短信通知，发送到哪个手机号' default '' ")
 	public String getPhone() {
 		return phone;
@@ -56,7 +72,8 @@ public class OrderNotice extends BaseEntity implements java.io.Serializable{
 
 	@Override
 	public String toString() {
-		return "PayNotice [id=" + id + ", isUse=" + isUse + ", phone=" + phone + "]";
+		return "OrderNotice [id=" + id + ", payNotice=" + payNotice + ", refundNotice=" + refundNotice + ", phone="
+				+ phone + "]";
 	}
 	
 }
