2cba6b2d722afa396ed4a46939b73c572fb0d2d1
Administrator <Administrator@GUANLEIMING-WIN11>
2022-02-15 09:23:22 +0800

    APIDoc优化

--- src/main/java/com/ApiDoc.java
+++ src/main/java/com/ApiDoc.java
@@ -5,13 +5,13 @@ import com.xnx3.doc.JavaDoc;
 public class ApiDoc {
 	public static void main(String[] args) {
 		JavaDoc doc = new com.xnx3.doc.JavaDoc("com.xnx3.wangmarket.shop.api.controller");
-//		doc.templatePath = "/Users/apple/Downloads/javadoc/";
-		doc.templatePath = "http://res.zvo.cn.obs.cn-north-4.myhuaweicloud.com/javadoc/v1.8/";
+		//doc.templatePath = "/Users/apple/Downloads/javadoc/";
+		doc.templatePath = "http://res.zvo.cn.obs.cn-north-4.myhuaweicloud.com/javadoc/v1.10/";
 		
-//		doc.name = "网市场云商城-用户端-API文档";
-//		doc.domain = "http://shop.imall.net.cn";
-//		doc.version = "1.7";
-//		doc.token = ""; 
+		doc.name = "网市场云商城-用户端-API文档";
+		doc.welcome = "网市场云建站系统用户端API文档说明";
+		doc.domain = "http://shop.imall.net.cn";
+		doc.version = "1.13";
 //		
 		doc.generateHtmlDoc();
 	}
